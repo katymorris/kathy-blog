2faafb2aed77f3e4debca27ae5b44cf128e6171b
Morris <kathryn.morris@nuance.com>
2022-06-25 20:04:51 -0600

    first push

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Kathy Codes`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Kathy Morris`,
+      summary: `is a Software engineer working in innovation and developer relations.`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `Kathy Morris Blog`,
+    siteUrl: `https://kathyblog.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      github: `katymorris`,
     },
   },
   plugins: [
--- src/components/bio.js
+++ src/components/bio.js
@@ -32,23 +32,9 @@ const Bio = () => {
 
   return (
     <div className="bio">
-      <StaticImage
-        className="bio-avatar"
-        layout="fixed"
-        formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
-        width={50}
-        height={50}
-        quality={95}
-        alt="Profile picture"
-      />
       {author?.name && (
         <p>
-          Written by <strong>{author.name}</strong> {author?.summary || null}
-          {` `}
-          <a href={`https://twitter.com/${social?.twitter || ``}`}>
-            You should follow them on Twitter
-          </a>
+          <strong>{author.name}</strong> {author?.summary || null}
         </p>
       )}
     </div>
